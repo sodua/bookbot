5db9275854ff6989fb4cddde8e587c6cd45a7c05
ct hildreth <cthildreth@gmail.com>
2025-11-17 21:08:43 -0500

    cleaning up output messages

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ def get_book_text(path_to_file):
         return file_contents
 
 def main():
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
     get_num_words(get_book_text("./books/frankenstein.txt"))
+    print("--------- Character Count -------")
     sorted_list = count_chars(get_book_text("./books/frankenstein.txt"))
 
 main()
